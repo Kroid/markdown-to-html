c1577362438dc4279dbfbac1d28989d47a60bf49
George Mironov <kroid@yandex.ru>
2021-02-13 12:41:26 +0300

    clear logs

--- src/links-plugin.js
+++ src/links-plugin.js
@@ -1,21 +1,14 @@
-require('node-json-color-stringify')
-
 var visit = require('unist-util-visit')
 
 function links(files) {
   backlinks = {}
 
   function plugin() {
-    // console.log(backlinks);
 
     function transformer(tree, file) {
-      // console.log(JSON.stringify(tree, null, 2));
       visit(tree, 'wikiLink', visitor)
 
-      function visitor(node) {
-        // console.log(node)
-        // console.log(JSON.colorStringify(node, null, 2));
-      }
+      function visitor(node) { }
     }
     return transformer;
   }
