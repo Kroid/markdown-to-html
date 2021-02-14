6d03d0c9df30c004d1bf24656343e86126ce21d7
George Mironov <kroid@yandex.ru>
2021-02-14 15:39:43 +0300

    change absolute links to relative

--- src/links-plugin.js
+++ src/links-plugin.js
@@ -8,7 +8,11 @@ function links(files) {
     function transformer(tree, file) {
       visit(tree, 'wikiLink', visitor)
 
-      function visitor(node) { }
+      function visitor(node) {
+        if (node.data.hProperties.href[0] == '/') {
+          node.data.hProperties.href = node.data.hProperties.href.substring(1)
+        }
+      }
     }
     return transformer;
   }
